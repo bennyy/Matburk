2955eb8ea84654414b1c5f8fda37ed7339490e9b
bennyy <4161184+bennyy@users.noreply.github.com>
2026-01-28 20:37:56 +0100

    Change tab names

--- frontend/src/components/PlannerGrid/PlannerGrid.jsx
+++ frontend/src/components/PlannerGrid/PlannerGrid.jsx
@@ -368,7 +368,7 @@ export default function PlannerGrid({
                     d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2"
                   />
                 </svg>
-                <span>Matlådor</span>
+                <span>Veckoplan</span>
               </span>
               {activeTab === TABS.PLANNING && (
                 <div className="absolute bottom-0 left-0 right-0 h-0.5 bg-blue-600"></div>
@@ -393,7 +393,7 @@ export default function PlannerGrid({
                     d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"
                   />
                 </svg>
-                <span>Bibliotek</span>
+                <span>Recept</span>
               </span>
               {activeTab === TABS.LIBRARY && (
                 <div className="absolute bottom-0 left-0 right-0 h-0.5 bg-blue-600"></div>
